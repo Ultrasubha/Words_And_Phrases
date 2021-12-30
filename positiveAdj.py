6d614cfd3aedab8e3e2044a38e6be3b0024cde84
Subhadeep Mandal <ultrasubha@gmail.com>
2021-12-30 15:08:30 +0530

    Create positiveAdj.py

---
+++ positiveAdj.py
@@ -0,0 +1,44 @@
+"""
+Program: Dictionary that enlists Positive Adjectives to address Dignitaries
+Created by Subhadeep Mandal
+On 11.11.2021
+Feel free to add more to this Dictionary
+"""
+Adjectives = {
+		"Calm" : "",
+		"Humble" : "",
+		"Charismatic" : "",
+		"Eclectic" : "possessing a style that is inspired by varying ideas and cultures",
+		"Unassuming" : "Not arrogant",
+		"Modest" : "",
+		"Witty" : "",
+		"Clever" : "",
+		"Generous" : "",
+		"Creative" : "",
+		"Flexible" : "",
+		"Adaptable" : "",
+		"Affectionate" : "",
+		"Ambitious" : "",
+		"Amiable" : "Diffusing warmth and friendliness",
+		"Compassionate" : "",
+		"Considerate" : "",
+		"Courteous" : "",
+		"Diligent" : "",
+		"Elated" : "",
+		"Empathetic" : "",
+		"Sympathetic" : "",
+		"Generous" : "",
+		"Gregarious" : "Temperamentally seeking and enjoying the company of others",
+		"Intuitive" : "",
+		"Inventive" : "",
+		"Persistent" : "",
+		"Philosophical" : "",
+		"Rational" : "",
+		"Reliable" : "",
+		"Resourceful" : "",
+		"Sincere" : ""
+}
+
+#print(Adjectives.keys())
+for key in Adjectives.keys():
+  print(key)
